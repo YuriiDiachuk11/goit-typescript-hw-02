2d313c56133798d5c62bd25869b7011e33834a49
YD <yuriidiachuk@icloud.com>
2024-12-27 16:12:48 +0200

    change api.js

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
+import { ApiResponse } from "../components/App/App.types";
 
-const fetchImages = async (query, page) => {
-  const response = await axios.get(
+const fetchImages = async (
+  query: string,
+  page: number
+): Promise<ApiResponse> => {
+  const response = await axios.get<ApiResponse>(
     `https://api.unsplash.com/search/photos?client_id=5UD8DYrMxBo-IEihraDNL4h45xfwBbotn8LZ1CUQDX4&query=${query}&page=${page}&per_page=10`
   );
   return response.data;
